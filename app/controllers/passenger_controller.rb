4acfaa5cc4e8cb406bb05dc345542b2420725bcf
Pedro ivan <pedro92_0224@hotmail.com>
2017-09-23 01:03:58 -0500

    add passenger model and view

--- app/controllers/passenger_controller.rb
+++ app/controllers/passenger_controller.rb
@@ -1,5 +1,5 @@
 class PassengerController < ApplicationController
-
+  before_action :authenticate_user!
 
   def index
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922045415) do
+ActiveRecord::Schema.define(version: 20170923054246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 20170922045415) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "licence_plate", null: false
+    t.string "color",         null: false
+    t.string "brand_car",     null: false
+  end
 
   add_foreign_key "passengers", "users"
 end
